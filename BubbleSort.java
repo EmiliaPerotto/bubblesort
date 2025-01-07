b10eed7dbbaed2f5dcf9f7559b95333edd1fb4c4
Emilia Perotto <100068661@csisd.org>
2025-01-07 14:55:00 -0600

    dfb

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class BubbleSort{
+  public static void bubble(int[] arr) {
+    for (int i = 0; i < arr.length; i++){
+      for (int a = 0; a < arr.length; a++){
+      if(arr[i] <= arr[a]){
+        int val1 = arr[i];
+        int val2 = arr[a];
+        arr[i] = arr[a];
+        arr[a] = val1;
+      }
+      }
+    }  
+  } 
+  public static void main(String[] args) throws IOException{
+    Scanner in = new Scanner(System.in);
+    System.out.println("Input filename: ");
+    String filename = in.next();
+    Scanner fin = new Scanner(new File(filename));
+    int lines = fin.nextInt();
+    int[] arr = new int[lines];
+    int j = 0;
+    while(fin.hasNextLine()){
+      arr[j] = fin.nextInt();
+      j++;
+    }
+    bubble(arr);
+    System.out.println("Bubble sort: ");
+    for (int i = 0; i < arr.length; i++){
+      if(i != arr.length -1){
+        System.out.print(arr[i] +" , ");
+      }
+      else{
+        System.out.print(arr[i]);
+    }
+    }
+
+}
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # bubblesort
+
+## Due: Wed 1/8 at 11:59 PM
+
+### No Googling the Bubble sorting algorithm
+
+- Create a program called `BubbleSort.java`
+- Create a void static method that implents the bubble sort algorithm
+- In the main method:
+  - Prompt the user to enter a filename containing the data to read in for bubble sort
+  - Read from the file
+  - The first line in the file is the number of elements the file contains
+  - Read the remaining lines of the file into an array
+  - Use the bubble sort method you wrote to sort the array
+  - Print the sorted array
+
+***Example Input:***\
+input1.txt\
+
+***Example Output:***\
+Bubble sort: 1, 2, 3, 7, 17, 19, 24, 30, 44, 85\
